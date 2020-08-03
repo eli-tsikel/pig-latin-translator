a8279e8bf973eeb223458eb1ba3b07117ed2c1b9
Ilya Tsikel <ilya.tsikel@oracle.com>
2020-08-03 18:49:59 +0200

    Creating skeleton application

---
+++ pig-latin-translator-app/src/main/java/com/citrix/task/translator/CliApplication.java
@@ -0,0 +1,13 @@
+package com.citrix.task.translator;
+
+import java.io.IOException;
+
+/**
+ * CLI application that reads text from cmd line and prints translated text to pig latin.
+ */
+public class CliApplication {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+}
